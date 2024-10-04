caaea3dc2db949101b05873b30736f177f42c70a
kiruba2602 <kirubakaran260202@gmail.com>
2024-10-04 13:10:02 +0530

    updated file

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8oayaz+2@-8-c1da_tga2_g72dvnvjsyjdh4_p2(tyn)(!-i7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-rc4zmetz.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
